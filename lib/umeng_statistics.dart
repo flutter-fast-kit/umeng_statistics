86d60679fd9c57f35c6b61ce8bff65287eb219af
SimMan <liwei0990@gmail.com>
2021-05-26 17:53:31 +0800

    support null safety

--- lib/umeng_statistics.dart
+++ lib/umeng_statistics.dart
@@ -4,7 +4,7 @@ import 'dart:collection';
 import 'package:flutter/services.dart';
 
 class UmengStatistics {
-  static const MethodChannel _channel = const MethodChannel('umeng_statistics');
+  static const MethodChannel _channel = MethodChannel('umeng_statistics');
 
   /// 初始化Appkey
   ///
@@ -14,73 +14,59 @@ class UmengStatistics {
   /// * logEnabled ： 开启日志
   /// * encryptEnabled ：加密
   /// * processEvent ：子进程中对自定义事件进行统计
-  static Future<bool> initAppKey(
-      String androidAppKey, String iosAppKey, String channel,
-      {bool logEnabled = false,
-      bool encryptEnabled = false,
-      processEvent = false}) async {
-    Map<String, dynamic> params = HashMap();
+  static Future<bool?> initAppKey(String androidAppKey, String iosAppKey, String channel,
+      {bool logEnabled = false, bool encryptEnabled = false, bool processEvent = false}) async {
+    final Map<String, dynamic> params = HashMap<String, dynamic>();
     params['android'] = androidAppKey;
     params['ios'] = iosAppKey;
     params['channel'] = channel;
     params['logEnabled'] = logEnabled;
     params['encryptEnabled'] = encryptEnabled;
     params['processEvent'] = processEvent;
-    final dynamic result = await _channel.invokeMethod("initAppKey", params);
-    return result;
+    return await _channel.invokeMethod<bool>('initAppKey', params);
   }
 
   /// 事件统计功能
-  static Future<bool> onEvent(String id, Map<String, Object> data) async {
-    Map<String, Object> params = HashMap();
+  static Future<bool?> onEvent(String id, Map<String, Object> data) async {
+    final Map<String, Object> params = HashMap();
     params['id'] = id;
     params['data'] = data;
-    final dynamic result = await _channel.invokeMethod("onEvent", params);
-    return result;
+    return await _channel.invokeMethod<bool>('onEvent', params);
   }
 
   /// 设置用户账户
-  static Future<bool> onProfileSignIn(String userID) async {
-    Map<String, Object> params = HashMap();
+  static Future<bool?> onProfileSignIn(String userID) async {
+    final Map<String, Object> params = HashMap();
     params['userID'] = userID;
-    final dynamic result =
-        await _channel.invokeMethod("onProfileSignIn", params);
-    return result;
+    return await _channel.invokeMethod<bool>('onProfileSignIn', params);
   }
 
   /// 取消用户账户
-  static Future<bool> onProfileSignOff() async {
-    final dynamic result = await _channel.invokeMethod("onProfileSignOff");
-    return result;
+  static Future<bool?> onProfileSignOff() async {
+    return await _channel.invokeMethod<bool>('onProfileSignOff');
   }
 
   /// 自动采集页面
-  static Future<bool> setPageCollectionModeAuto() async {
-    final dynamic result =
-        await _channel.invokeMethod("setPageCollectionModeAuto");
-    return result;
+  static Future<bool?> setPageCollectionModeAuto() async {
+    return await _channel.invokeMethod('setPageCollectionModeAuto');
   }
 
   /// 手动采集页面
-  static Future<bool> setPageCollectionModeManual() async {
-    final dynamic result =
-        await _channel.invokeMethod("setPageCollectionModeManual");
-    return result;
+  static Future<bool?> setPageCollectionModeManual() async {
+    return await _channel.invokeMethod<bool>('setPageCollectionModeManual');
   }
 
   /// 自动页面时长统计，必须配对使用
-  static Future<bool> onPageStart(String viewName) async {
-    Map<String, Object> params = HashMap();
+  static Future<bool?> onPageStart(String viewName) async {
+    final Map<String, Object> params = HashMap();
     params['viewName'] = viewName;
-    final dynamic result = await _channel.invokeMethod("onPageStart", params);
-    return result;
+    return await _channel.invokeMethod<bool>('onPageStart', params);
   }
 
   /// 结束页面时长统计，必须配对使用
-  static Future<bool> onPageEnd(String viewName) async {
-    Map<String, Object> params = HashMap();
+  static Future<bool?> onPageEnd(String viewName) async {
+    final Map<String, Object> params = HashMap();
     params['viewName'] = viewName;
-    final dynamic result = await _channel.invokeMethod("onPageEnd", params);
-    return result;
+    return await _channel.invokeMethod<bool>('onPageEnd', params);
   }
 }
